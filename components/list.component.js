44ecac32a5647f5028b0d77dc53fac0735331b18
Reza Julian <Reza.Julian-EXT@xapiens.id>
2020-09-10 21:47:05 +0700

    change classname color

--- components/list.component.js
+++ components/list.component.js
@@ -78,17 +78,7 @@ const List = ({ filter }) => {
                             <span className="font font-sans text-white text-2xl font-bold mb-2">{row.name}</span>
                             <div className="flex flex-row justify-around">
                                 {row.types.map((row, index) => (
-                                    <div key={index} className={`
-                                    ${
-                                        !_isEmpty(_find(dataType, ["type", row])) &&
-                                        _find(dataType, ["type", row]).color
-                                        }
-                                    flex
-                                    px-6
-                                    py-1
-                                    rounded-lg
-                                    content-center
-                                    `}>
+                                    <div key={index} className={`flex px-6 py-1 ${!_isEmpty(_find(dataType, ["type", row])) && _find(dataType, ["type", row]).color} rounded-lg content-center`}>
                                         <span className="font font-sans text-white text-xs">{row}</span>
                                     </div>
                                 ))}
